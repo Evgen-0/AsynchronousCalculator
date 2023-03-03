ca2a26c56b279014535ff2930c17aca4f34a43ca
Evgen-0 <116595114+Evgen-0@users.noreply.github.com>
2023-03-04 00:05:04 +0200

    Add function 'Add'

--- main.go
+++ main.go
@@ -1,37 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	//scanner := bufio.NewScanner(os.Stdin)
-	//for scanner.Scan() {
-	//	txt := scanner.Text()
-	//	AddText(txt)
-	//	fmt.Printf("Эхо: %s\n", txt)
-	//	if txt == "0" {
-	//		return
-	//	}
-	//}
-
-	expression := "k+k"
-	res, err := AddText(expression)
-	if err != nil {
-		fmt.Println(err)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Ведіть вираз")
+	for scanner.Scan() {
+		txt := scanner.Text()
+		fmt.Printf("Відповідь: %d\n", Add(txt))
+		fmt.Println("Ведіть вираз")
 	}
-	fmt.Println(res)
 }
 
-func AddText(txt string) (int, error) {
+func Add(txt string) int {
 	resArr := strings.Split(txt, "+")
-	res, err := Add(resArr[0], resArr[1])
-	return res, err
+	var expr1 string
+	var expr2 string
+	if len(resArr) == 2 {
+		expr1 = resArr[0]
+		expr2 = resArr[1]
+	}
+	res, err := AddExpression(expr1, expr2)
+	if err != nil {
+		err := fmt.Errorf("помилка")
+		fmt.Println(err)
+		return 0
+	}
+	return res
 }
 
-func Add(expr1 string, expr2 string) (int, error) {
+func AddExpression(expr1 string, expr2 string) (int, error) {
+	//TODO Переписати так щоб можна було необмежену кількість пераметрів передавати з одним знаком.
 	res1, err := strconv.Atoi(expr1)
 	res2, err := strconv.Atoi(expr2)
 	return res1 + res2, err
