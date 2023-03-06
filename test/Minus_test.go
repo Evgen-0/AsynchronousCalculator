3e16ae74e54a6c14eefa62a22d99a57d52fc17f0
Evgen-0 <116595114+Evgen-0@users.noreply.github.com>
2023-03-06 12:51:50 +0200

    Tests for the "Minus" function

---
+++ test/Minus_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"github.com/Evgen-0/AsynchronousCalculator"
+	"testing"
+)
+
+func TestMinus(t *testing.T) {
+	t.Run("Correct", func(t *testing.T) {
+		t.Parallel()
+		expression := "50-30"
+		result := 20
+		realResult := main.NoName(expression)
+		if realResult != result {
+			t.Errorf("expected result: %d != %d real result", result, realResult)
+		}
+	})
+	t.Run("Bad", func(t *testing.T) {
+		t.Parallel()
+		expression := "10+k"
+		realResult := main.NoName(expression)
+		result := 0
+		if realResult != result {
+			t.Errorf("expected realResult: %d != %d real realResult", realResult, result)
+		}
+	})
+	t.Run("panic", func(t *testing.T) {
+		t.Parallel()
+		expression := "1"
+		result := 0
+		realResult := main.NoName(expression)
+		if realResult != result {
+			t.Errorf("panic")
+		}
+
+	})
+}
