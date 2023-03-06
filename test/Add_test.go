7f836f737c39e65c253c37d8918c24f0033c5c27
Evgen-0 <116595114+Evgen-0@users.noreply.github.com>
2023-03-06 13:04:38 +0200

    Tests for the "Add" function

--- test/Add_test.go
+++ test/Add_test.go
@@ -1,13 +1,16 @@
-package main
+package test
 
-import "testing"
+import (
+	"github.com/Evgen-0/AsynchronousCalculator"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	t.Run("Correct", func(t *testing.T) {
 		t.Parallel()
 		expression := "10+10"
 		result := 20
-		realResult := Add(expression)
+		realResult := main.NoName(expression)
 		if realResult != result {
 			t.Errorf("expected result: %d != %d real result", result, realResult)
 		}
@@ -15,7 +18,7 @@ func TestAdd(t *testing.T) {
 	t.Run("Bad", func(t *testing.T) {
 		t.Parallel()
 		expression := "10+k"
-		realResult := Add(expression)
+		realResult := main.NoName(expression)
 		result := 0
 		if realResult != result {
 			t.Errorf("expected realResult: %d != %d real realResult", realResult, result)
@@ -25,7 +28,7 @@ func TestAdd(t *testing.T) {
 		t.Parallel()
 		expression := "1"
 		result := 0
-		realResult := Add(expression)
+		realResult := main.NoName(expression)
 		if realResult != result {
 			t.Errorf("panic")
 		}
